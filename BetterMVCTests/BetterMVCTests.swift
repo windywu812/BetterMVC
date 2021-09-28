bffd81d9939d5be8e698724d0c2247737ed16157
Windy <windywu812@gmail.com>
2021-09-28 11:44:58 +0700

    adding unit test

--- BetterMVCTests/BetterMVCTests.swift
+++ BetterMVCTests/BetterMVCTests.swift
@@ -10,24 +10,23 @@ import XCTest
 
 class BetterMVCTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_getAllGames_inViewDidLoad() {
+        
+        let expectation = expectation(description: "Test Get All Games")
+        
+        let sut = ViewController()
+        sut.loadViewIfNeeded()
+        sut.getAllGames()
+        
+        XCTAssert(sut.games.count == 0)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            XCTAssert(!sut.games.isEmpty)
+            XCTAssertFalse(sut.loadingIndicator.isAnimating)
+            expectation.fulfill()
         }
+        
+        waitForExpectations(timeout: 5)
     }
 
 }
